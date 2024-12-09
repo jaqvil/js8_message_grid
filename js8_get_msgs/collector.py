26f6f06d2808f890b81e9380334cae4b575e58a2
jaqvil <109920375+jaqvil@users.noreply.github.com>
2024-12-09 19:50:39 +0100

    1st few files fixed
    Related to #5
    
    Code reformatted in 1st 5 files
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/jaqvil/js8_message_grid/issues/5?shareId=XXXX-XXXX-XXXX-XXXX).

--- js8_get_msgs/collector.py
+++ js8_get_msgs/collector.py
@@ -1,210 +1,170 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-import os
-import sys
 import time
 import json
 import argparse
 import maidenhead as mh
 from js8net import *
-#import pdb
 import requests
 import uuid
-import json
-import re
+import threading
+from queue import Queue
 
-global js8host
-global js8port
-global mycall
-global mygrid
-global myfreq
-global myspeed
-global myuuid
-global radio
-global aggregator
+def update_thread(name, js8host, js8port, myuuid, aggregator):
+    """
+    Thread to update station information and send it to the aggregator.
 
-global station_lock
-global cmd_queue
-global cmd_lock
-station_lock=threading.Lock()
-cmd_queue=Queue()
-cmd_lock=threading.Lock()
-
-global tx_allowed
-tx_allowed=False
-
-def update_thread(name):
-    global js8host
-    global js8port
-    global mycall
-    global mygrid
-    global myfreq
-    global myspeed
-    global myuuid
-    print(name+' started...')
-    last=time.time()
-    while(True):
+    Args:
+        name (str): Name of the thread.
+        js8host (str): Hostname of the JS8Call server.
+        js8port (int): Port of the JS8Call server.
+        myuuid (str): UUID of the station.
+        aggregator (str): Aggregator to send traffic to.
+    """
+    global mycall, mygrid, myfreq, myspeed
+    print(name + ' started...')
+    last = time.time()
+    while True:
         with station_lock:
-            mycall=get_callsign()
-            mygrid=get_grid()
-            myfreq=get_freq()
-            myspeed=get_speed()
-        if(time.time()>=last+30):
-            last=time.time()
-            act=get_call_activity()
-            if(act):
-                stuff=[]
+            mycall = get_callsign()
+            mygrid = get_grid()
+            myfreq = get_freq()
+            myspeed = get_speed()
+        if time.time() >= last + 30:
+            last = time.time()
+            act = get_call_activity()
+            if act:
+                stuff = []
                 for a in act:
-                    stuff.append({'call':a.call,'snr':a.snr,'utc':a.utc,'grid':a.grid})
-                res=requests.post('http://'+aggregator+'/collect',json={'type':'grids','uuid':myuuid,'stuff':stuff})
+                    stuff.append({'call': a.call, 'snr': a.snr, 'utc': a.utc, 'grid': a.grid})
+                res = requests.post('http://' + aggregator + '/collect', json={'type': 'grids', 'uuid': myuuid, 'stuff': stuff})
                 print(res)
                 res.close()
         time.sleep(5.0)
 
-def traffic_thread(name):
-    global aggregator
-    global myuuid
-    print(name+' started...')
-    while(True):
+def traffic_thread(name, aggregator, myuuid):
+    """
+    Thread to handle incoming traffic and send it to the aggregator.
+
+    Args:
+        name (str): Name of the thread.
+        aggregator (str): Aggregator to send traffic to.
+        myuuid (str): UUID of the station.
+    """
+    print(name + ' started...')
+    while True:
         time.sleep(0.1)
-        if(not(rx_queue.empty())):
+        if not rx_queue.empty():
             with rx_lock:
-                rx=rx_queue.get()
+                rx = rx_queue.get()
             print('New traffic received...')
-            if(rx['type']=='RX.DIRECTED'):
+            if rx['type'] == 'RX.DIRECTED':
                 print('Sending traffic to aggregator...')
                 print(rx)
-                rx['uuid']=myuuid
-                res=requests.post('http://'+aggregator+'/collect',json={'type':'traffic','stuff':rx})
+                rx['uuid'] = myuuid
+                res = requests.post('http://' + aggregator + '/collect', json={'type': 'traffic', 'stuff': rx})
                 print(res)
                 res.close()
             else:
                 print('Not sending traffic to aggregator...')
                 print(rx)
 
-def station_thread(name):
-    global aggregator
-    global myuuid
-    global mycall
-    global mygrid
-    global js8host
-    global js8port
-    global myspeed
-    global myfreq
-    global radio
-    global tx_allowed
-    print(name+' started...')
+def station_thread(name, js8host, js8port, myuuid, aggregator, radio, tx_allowed):
+    """
+    Thread to update station information and send it to the aggregator.
+
+    Args:
+        name (str): Name of the thread.
+        js8host (str): Hostname of the JS8Call server.
+        js8port (int): Port of the JS8Call server.
+        myuuid (str): UUID of the station.
+        aggregator (str): Aggregator to send traffic to.
+        radio (str): Type/model of radio.
+        tx_allowed (bool): Whether the station is allowed to transmit.
+    """
+    global mycall, mygrid, myspeed, myfreq
+    print(name + ' started...')
     time.sleep(7.5)
-    while(True):
+    while True:
         with station_lock:
-            if(mygrid and mygrid!=''):
-                if(len(mygrid)>8):
-                    (lat,lon)=mh.to_location(mygrid[0:8])
+            if mygrid and mygrid != '':
+                if len(mygrid) > 8:
+                    lat, lon = mh.to_location(mygrid[0:8])
                 else:
-                    (lat,lon)=mh.to_location(mygrid)
+                    lat, lon = mh.to_location(mygrid)
             else:
-                lat=False
-                lon=False
-            j={'time': time.time(),
-               'en_time': time.asctime(),
-               'call': mycall,
-               'grid': mygrid,
-               'lat': lat,
-               'lon': lon,
-               'host': js8host,
-               'port': js8port,
-               'speed': myspeed,
-               'dial': myfreq['dial'],
-               'carrier': myfreq['offset'],
-               'radio': radio,
-               'tx':tx_allowed}
-            res=requests.post('http://'+aggregator+'/collect',json={'type':'station','uuid':myuuid,'stuff':j})
+                lat = False
+                lon = False
+            j = {
+                'time': time.time(),
+                'en_time': time.asctime(),
+                'call': mycall,
+                'grid': mygrid,
+                'lat': lat,
+                'lon': lon,
+                'host': js8host,
+                'port': js8port,
+                'speed': myspeed,
+                'dial': myfreq['dial'],
+                'carrier': myfreq['offset'],
+                'radio': radio,
+                'tx': tx_allowed
+            }
+            res = requests.post('http://' + aggregator + '/collect', json={'type': 'station', 'uuid': myuuid, 'stuff': j})
             print('sending station')
             print(res)
-#            j=res.json() # these are basically placeholders for sending commands back to the radios
-#            print(j)
             res.close()
         time.sleep(15)
-                        
-# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 # Main program.
-if(__name__ == '__main__'):
-    parser=argparse.ArgumentParser(description='JS8call data collector.')
-    parser.add_argument('--js8_host',default=False,help='IP/DNS of JS8Call server (default localhost)')
-    parser.add_argument('--js8_port',default=False,help='TCP port of JS8Call server (default 2442)')
-    parser.add_argument('--aggregator',default=False,help='Aggregator to send traffic to (default is localhost:8000)')
-    parser.add_argument('--uuid',default=False,help='Use a specific UUID (default is auto-generate)')
-    parser.add_argument('--radio',default=False,help='Type/model of radio (used for display only)')
-    parser.add_argument('--transmit',default=False,help='This station allowed to transmit (default is rx only)',
-                        action='store_true')
-    args=parser.parse_args()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='JS8call data collector.')
+    parser.add_argument('--js8_host', default=False, help='IP/DNS of JS8Call server (default localhost)')
+    parser.add_argument('--js8_port', default=False, help='TCP port of JS8Call server (default 2442)')
+    parser.add_argument('--aggregator', default=False, help='Aggregator to send traffic to (default is localhost:8000)')
+    parser.add_argument('--uuid', default=False, help='Use a specific UUID (default is auto-generate)')
+    parser.add_argument('--radio', default=False, help='Type/model of radio (used for display only)')
+    parser.add_argument('--transmit', default=False, help='This station allowed to transmit (default is rx only)', action='store_true')
+    args = parser.parse_args()
 
-    js8host=False
-    js8port=False
+    js8host = args.js8_host if args.js8_host else 'localhost'
+    js8port = int(args.js8_port) if args.js8_port else 2442
+    aggregator = args.aggregator if args.aggregator else 'localhost:8000'
+    radio = args.radio if args.radio else 'unknown'
+    myuuid = args.uuid if args.uuid else str(uuid.uuid4())
+    tx_allowed = args.transmit
 
-    mycall='unknown'
-    mygrid='unknown'
-    myfreq={'dial': 0, 'freq': 0, 'offset': 0}
-    myspeed='0'
-    
-    tx_allowed=args.transmit
-
-    if(args.js8_host):
-        js8host=args.js8_host
-    else:
-        js8host='localhost'
-
-    if(args.js8_port):
-        js8port=int(args.js8_port)
-    else:
-        js8port=2442
-
-    if(args.aggregator):
-        aggregator=args.aggregator
-    else:
-        aggregator='localhost:8000'
-
-    if(args.radio):
-        radio=args.radio
-    else:
-        radio='unknown'
-
-    if(args.uuid):
-        myuuid=args.uuid
-    else:
-        myuuid=str(uuid.uuid4())
+    mycall = 'unknown'
+    mygrid = 'unknown'
+    myfreq = {'dial': 0, 'freq': 0, 'offset': 0}
+    myspeed = '0'
 
     print('Connecting to JS8Call...')
-    start_net(js8host,js8port)
+    start_net(js8host, js8port)
     print('Connected.')
 
-    thread0=Thread(target=update_thread,args=('Update Thread',),daemon=True)
+    thread0 = Thread(target=update_thread, args=('Update Thread', js8host, js8port, myuuid, aggregator), daemon=True)
     thread0.start()
-    thread1=Thread(target=traffic_thread,args=('Traffic Thread',),daemon=True)
+    thread1 = Thread(target=traffic_thread, args=('Traffic Thread', aggregator, myuuid), daemon=True)
     thread1.start()
-    thread2=Thread(target=station_thread,args=('Station Thread',),daemon=True)
+    thread2 = Thread(target=station_thread, args=('Station Thread', js8host, js8port, myuuid, aggregator, radio, tx_allowed), daemon=True)
     thread2.start()
-    
-    while(True):
-        if(not(thread0.is_alive())):
+
+    while True:
+        if not thread0.is_alive():
             print('(re-)starting Update Thread...')
             thread0.join()
-            thread0=Thread(target=update_thread,args=('Update Thread',),daemon=True)
+            thread0 = Thread(target=update_thread, args=('Update Thread', js8host, js8port, myuuid, aggregator), daemon=True)
             thread0.start()
-        if(not(thread1.is_alive())):
+        if not thread1.is_alive():
             print('(re-)starting Traffic Thread...')
             thread1.join()
-            thread1=Thread(target=traffic_thread,args=('Traffic Thread',),daemon=True)
+            thread1 = Thread(target=traffic_thread, args=('Traffic Thread', aggregator, myuuid), daemon=True)
             thread1.start()
-        if(not(thread2.is_alive())):
+        if not thread2.is_alive():
             print('(re-)starting Station Thread...')
             thread2.join()
-            thread2=Thread(target=station_thread,args=('Station Thread',),daemon=True)
+            thread2 = Thread(target=station_thread, args=('Station Thread', js8host, js8port, myuuid, aggregator, radio, tx_allowed), daemon=True)
             thread2.start()
         time.sleep(1.0)
-
-#    pdb.set_trace()
-
-# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
--- js8_get_msgs/example.py
+++ js8_get_msgs/example.py
@@ -2,105 +2,78 @@
 # coding: utf-8
 
 import os
-import sys
 import time
 import json
 import argparse
 from os.path import exists
 from js8net import *
 
-# Main program.
-if(__name__ == '__main__'):
-    parser=argparse.ArgumentParser(description="Example of using js8net.py")
-    parser.add_argument("--js8_host",default=False,help="IP/DNS of JS8Call server (default localhost, env: JS8HOST)")
-    parser.add_argument("--js8_port",default=False,help="TCP port of JS8Call server (default 2442, env: JS8PORT)")
-    parser.add_argument("--clean",default=False,action="store_true",help="Start with clean spots (ie, don't load spots.json)")
-    parser.add_argument("--env",default=False,action="store_true",help="Use environment variables (cli options override)")
-    parser.add_argument("--listen",default=False,action="store_true",help="Listen only - do not write files")
-    parser.add_argument("--verbose",default=False,action="store_true",help="Lots of status messages")
-    args=parser.parse_args()
+def main():
+    """
+    Main function to demonstrate the usage of js8net.py.
+    """
+    parser = argparse.ArgumentParser(description="Example of using js8net.py")
+    parser.add_argument("--js8_host", default=False, help="IP/DNS of JS8Call server (default localhost, env: JS8HOST)")
+    parser.add_argument("--js8_port", default=False, help="TCP port of JS8Call server (default 2442, env: JS8PORT)")
+    parser.add_argument("--clean", default=False, action="store_true", help="Start with clean spots (ie, don't load spots.json)")
+    parser.add_argument("--env", default=False, action="store_true", help="Use environment variables (cli options override)")
+    parser.add_argument("--listen", default=False, action="store_true", help="Listen only - do not write files")
+    parser.add_argument("--verbose", default=False, action="store_true", help="Lots of status messages")
+    args = parser.parse_args()
 
     # Load spots.json for some historical context, unless the file is
     # missing, or the user asks not to.
-    if(exists("spots.json") and not(args.clean)):
+    if exists("spots.json") and not args.clean:
         with spots_lock:
-            f=open("spots.json")
-            spots=json.load(f)
-            f.close()
+            with open("spots.json") as f:
+                spots = json.load(f)
 
-    js8host=False
-    js8port=False
+    js8host = args.js8_host if args.js8_host else os.environ.get("JS8HOST") if args.env else "localhost"
+    js8port = int(args.js8_port) if args.js8_port else int(os.environ.get("JS8PORT")) if args.env else 2442
 
-    # If the user specified a command-line flag, that takes
-    # priority. If they also specified --env, any parameters they did
-    # not specify explicit flags for, try to grab from the
-    # environment.
-    if(args.js8_host):
-        js8host=args.js8_host
-    elif(os.environ.get("JS8HOST") and args.env):
-        js8host=os.environ.get("JS8HOST")
-    else:
-        js8host="localhost"
-
-    if(args.js8_port):
-        js8port=args.js8_port
-    elif(os.environ.get("JS8PORT") and args.env):
-        js8port=int(os.environ.get("JS8PORT"))
-    else:
-        js8port=2442
-
-    if(args.verbose):
+    if args.verbose:
         print("Connecting to JS8Call...")
-    start_net(js8host,js8port)
-    if(args.verbose):
+    start_net(js8host, js8port)
+    if args.verbose:
         print("Connected.")
-    print("Call: ",get_callsign())
-    print("Grid: ",get_grid())
-    print("Info: ",get_info())
-    print("Freq: ",get_freq())
-    print("Speed: ",get_speed())
-    print("Freq: ",set_freq(7078000,2000))
+    print("Call: ", get_callsign())
+    print("Grid: ", get_grid())
+    print("Info: ", get_info())
+    print("Freq: ", get_freq())
+    print("Speed: ", get_speed())
+    print("Freq: ", set_freq(7078000, 2000))
     get_band_activity()
-    
-    last=time.time()
-    while(True):
+
+    last = time.time()
+    while True:
         time.sleep(0.1)
-        if(not(rx_queue.empty())):
+        if not rx_queue.empty():
             with rx_lock:
-                rx=rx_queue.get()
-                if(not(args.listen)):
-                    f=open("rx.json","a")
-                    f.write(json.dumps(rx))
-                    f.write("\n")
-                    f.close()
-                    if(time.time()>=last+300):
-                        last=time.time()
-                        f=open("spots.json","w")
-                        f.write(json.dumps(spots))
+                rx = rx_queue.get()
+                if not args.listen:
+                    with open("rx.json", "a") as f:
+                        f.write(json.dumps(rx))
                         f.write("\n")
-                    f.close()
-                if(rx['type']=="RX.DIRECTED"):
-                    print("FROM:   ",rx['params']['FROM'])
-                    print("TO:     ",rx['params']['TO'])
-                    if('rxerror' in list(rx.keys())):
-                        print("RX ERR: ",rx['rxerror'])
-                    print("CMD:    ",rx['params']['CMD'])
-                    print("GRID:   ",rx['params']['GRID'])
-                    print("SPEED:  ",rx['params']['SPEED'])
-                    print("SNR:    ",rx['params']['SNR'])
-                    print("TDRIFT: ",str(int(rx['params']['TDRIFT']*1000)))
-                    print("DIAL:   ",rx['params']['DIAL'])
-                    print("OFFSET: ",rx['params']['OFFSET'])
-#                    print("FREQ:   ",rx['params']['FREQ'])
-                    print("EXTRA:  ",rx['params']['EXTRA'])
-                    print("TEXT:   ",rx['params']['TEXT'])
-#                    print("VALUE: ",rx['value'])
+                    if time.time() >= last + 300:
+                        last = time.time()
+                        with open("spots.json", "w") as f:
+                            f.write(json.dumps(spots))
+                            f.write("\n")
+                if rx['type'] == "RX.DIRECTED":
+                    print("FROM:   ", rx['params']['FROM'])
+                    print("TO:     ", rx['params']['TO'])
+                    if 'rxerror' in rx:
+                        print("RX ERR: ", rx['rxerror'])
+                    print("CMD:    ", rx['params']['CMD'])
+                    print("GRID:   ", rx['params']['GRID'])
+                    print("SPEED:  ", rx['params']['SPEED'])
+                    print("SNR:    ", rx['params']['SNR'])
+                    print("TDRIFT: ", str(int(rx['params']['TDRIFT'] * 1000)))
+                    print("DIAL:   ", rx['params']['DIAL'])
+                    print("OFFSET: ", rx['params']['OFFSET'])
+                    print("EXTRA:  ", rx['params']['EXTRA'])
+                    print("TEXT:   ", rx['params']['TEXT'])
                     print()
-#                    print(json.dumps(rx,indent=2,sort_keys=True))
-#                    print()
 
-# def mine(n):
-#     return(n['time'])
-
-# newest point with a grid
-# sorted(list(filter(lambda n: n['grid'],spots['N0GQ']['W5ODJ'])),key=mine)[0]
+if __name__ == '__main__':
+    main()
